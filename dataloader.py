fba644a6a91194e97f21154902a9d8be6a49396e
Furkan Mumcu <furkanmumcu2@gmail.com>
2022-07-08 05:24:44 -0400

    new dataloader added -from pt files

--- dataloader.py
+++ dataloader.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import datasets, transforms
 from torch.utils.data.sampler import SubsetRandomSampler
+from torchvision import models
 import numpy as np
 from os import path
 import os
@@ -11,35 +12,37 @@ import urllib
 import matplotlib.pyplot as plt
 import torchshow as ts
 from torchvision.datasets import ImageFolder
+from torch.utils.data.dataset import Dataset
 from typing import Any, Callable, cast, Dict, List, Optional, Tuple
 
 mu = [0.485, 0.456, 0.406]
 std = [0.229, 0.224, 0.225]
 
+#dics needed for imagenette
 lbl_dict_names = dict(
-    n01440764='tench',  # 0
-    n02102040='English springer',  # 217
-    n02979186='cassette player',  # 482
-    n03000684='chain saw',  # 491
-    n03028079='church',  # 497
-    n03394916='French horn',  # 566
-    n03417042='garbage truck',  # 569
-    n03425413='gas pump',  # 571
-    n03445777='golf ball',  # 574
-    n03888257='parachute'  # 701
+	n01440764='tench',  # 0
+	n02102040='English springer',  # 217
+	n02979186='cassette player',  # 482
+	n03000684='chain saw',  # 491
+	n03028079='church',  # 497
+	n03394916='French horn',  # 566
+	n03417042='garbage truck',  # 569
+	n03425413='gas pump',  # 571
+	n03445777='golf ball',  # 574
+	n03888257='parachute'  # 701
 )
 
 lbl_dict_ids = dict(
-    n01440764=0,  # 0
-    n02102040=217,  # 217
-    n02979186=482,  # 482
-    n03000684=491,  # 491
-    n03028079=497,  # 497
-    n03394916=566,  # 566
-    n03417042=569,  # 569
-    n03425413=571,  # 571
-    n03445777=574,  # 574
-    n03888257=701  # 701
+	n01440764=0,  # 0
+	n02102040=217,  # 217
+	n02979186=482,  # 482
+	n03000684=491,  # 491
+	n03028079=497,  # 497
+	n03394916=566,  # 566
+	n03417042=569,  # 569
+	n03425413=571,  # 571
+	n03445777=574,  # 574
+	n03888257=701  # 701
 )
 
 lbl_dict = dict([(0, 0), (1, 217), (2, 482), (3, 491), (4, 497), (5, 566), (6, 569), (7, 571), (8, 574), (9, 701)])
@@ -47,70 +50,91 @@ lbl_dict = dict([(0, 0), (1, 217), (2, 482), (3, 491), (4, 497), (5, 566), (6, 5
 
 class CustomImageFolder(ImageFolder):
 
-    def __getitem__(self, index: int) -> Tuple[Any, Any]:
-        """
-        Args:
-            index (int): Index
+	def __getitem__(self, index: int) -> Tuple[Any, Any]:
+		"""
+		Args:
+			index (int): Index
 
-        Returns:
-            tuple: (sample, target) where target is class_index of the target class.
-        """
-        path, target = self.samples[index]
-        sample = self.loader(path)
-        if self.transform is not None:
-            sample = self.transform(sample)
-        if self.target_transform is not None:
-            target = self.target_transform(target)
+		Returns:
+			tuple: (sample, target) where target is class_index of the target class.
+		"""
+		path, target = self.samples[index]
+		sample = self.loader(path)
+		if self.transform is not None:
+			sample = self.transform(sample)
+		if self.target_transform is not None:
+			target = self.target_transform(target)
 
-        #print(11111)
-        return sample, target
+		#print(11111)
+		return sample, target
 
 
 def get_loaders(dir):
-    #valdir = path.join(args.data_dir, 'val')
-    valdir = 'C:/Users/furkan/Desktop/projects/combine-attack/data/imagenette2-320-tiny50/val'
-    val_dataset = CustomImageFolder(dir,
-                                       transforms.Compose([transforms.Resize(224),
-                                                           transforms.CenterCrop(224),
-                                                           transforms.ToTensor(),
-                                                           transforms.Normalize(mean=mu, std=std)
-                                                           ]))
-    val_dataset.__getitem__(1)
-    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=10, shuffle=False,
-                                             num_workers=0, pin_memory=True)
-    return val_loader
+	#valdir = path.join(args.data_dir, 'val')
+	valdir = 'C:/Users/furkan/Desktop/projects/combine-attack/data/imagenette2-320-tiny50/val'
+	val_dataset = CustomImageFolder(dir,
+									transforms.Compose([transforms.Resize(224),
+														transforms.CenterCrop(224),
+														transforms.ToTensor(),
+														transforms.Normalize(mean=mu, std=std)
+														]))
+	val_dataset.__getitem__(1)
+	val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=10, shuffle=False,
+											 num_workers=0, pin_memory=True)
+	return val_loader
 
 
+class DataFromFile(Dataset):
+	def __init__(self, data_path, label_path):
+
+		#self.data = pd.read_csv(csv_path)
+		self.data = torch.load(data_path)
+
+		#self.labels = np.asarray(self.data.iloc[:, 0])
+		self.labels = torch.load(label_path)
+
+	def __getitem__(self, index):
+		single_image_data = self.data[index]
+		single_image_label = self.labels[index]
+
+		return single_image_data, single_image_label
+
+	def __len__(self):
+		return len(self.data)
+
+
+def get_loaders_v2(data_path, label_path):
+	dataset = DataFromFile(data_path, label_path)
+
+	dataset.__getitem__(1)
+	val_loader = torch.utils.data.DataLoader(dataset, batch_size=10, shuffle=False, num_workers=0, pin_memory=False)
+	return val_loader
+
+
+'''
 if __name__ == '__main__':
+	device = torch.device("cuda")
+	model = models.resnet50(pretrained=True).to(device)
+	model.eval()
+	correct = 0
+	total = 0
 
-    model = timm.create_model('vit_base_patch16_224', pretrained=True)
-    model = model.cuda()
-    model.eval()
+	data_path = 'data/adv-fgsm-pt-complete/data.pt'
+	label_path = 'data/adv-fgsm-pt-complete/labels.pt'
+	loader = get_loaders_v2(data_path, label_path)
 
-    url, filename = (
-    "https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt", "imagenet_classes.txt")
-    urllib.request.urlretrieve(url, filename)
-    with open("imagenet_classes.txt", "r") as f:
-        categories = [s.strip() for s in f.readlines()]
+	for i, (images, labels) in enumerate(loader):
+		print(str(i) + " of " + str(len(loader)))
 
-    images = get_loaders()
-    print(type(images))
+		images = images.to(device)
+		labels = labels.to(device)
 
-    #for i, ((X, y), (x1, y1)) in enumerate(zip(images, images)):
-    for i, (X, y) in enumerate(images):
-        print(i)
-        y = torch.ones(10) * lbl_dict[int(y[0])]
-        y = y.type(torch.long)
+		outputs = model(images)
 
-        X, y = X.cuda(), y.cuda()
+		_, pre = torch.max(outputs.data, 1)
 
-        with torch.no_grad():
-            out = model(X)
+		total += 1
+		correct += (pre == labels).sum()
 
-        probabilities = torch.nn.functional.softmax(out[0], dim=0)
-        print(probabilities.shape)
-
-        #ts.show(X[0])
-        top5_prob, top5_catid = torch.topk(probabilities, 5)
-        for i in range(top5_prob.size(0)):
-            print(categories[top5_catid[i]], top5_prob[i].item())
+	print('Accuracy of test text: %f %%' % (100 * float(correct) / (total * 10)))
+'''

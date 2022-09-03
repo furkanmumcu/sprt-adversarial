d42a85fa742b11d32fbb75e1eddf647c9c8fa05f
Furkan Mumcu <furkanmumcu2@gmail.com>
2022-09-02 22:51:33 -0400

    tests added

---
+++ tests.sh
@@ -0,0 +1,40 @@
+python attack-multi-label-only.py --target deit-s --threshold s --multi_case 5 --deit True --is_transform True
+python attack-multi-label-only.py --target deit-s --threshold m --multi_case 5 --deit True --is_transform True
+python attack-multi-label-only.py --target deit-s --threshold l --multi_case 5 --deit True --is_transform True
+python attack-multi-label-only.py --target deit-s --threshold xl --multi_case 5 --deit True --is_transform True
+
+python attack-multi-label-only.py --target deit-t --threshold s --multi_case 5 --deit True --is_transform True
+python attack-multi-label-only.py --target deit-t --threshold m --multi_case 5 --deit True --is_transform True
+python attack-multi-label-only.py --target deit-t --threshold l --multi_case 5 --deit True --is_transform True
+python attack-multi-label-only.py --target deit-t --threshold xl --multi_case 5 --deit True --is_transform True
+
+python attack-multi-label-only.py --target deit-b --threshold s --multi_case 5 --deit True --is_transform True
+python attack-multi-label-only.py --target deit-b --threshold m --multi_case 5 --deit True --is_transform True
+python attack-multi-label-only.py --target deit-b --threshold l --multi_case 5 --deit True --is_transform True
+python attack-multi-label-only.py --target deit-b --threshold xl --multi_case 5 --deit True --is_transform True
+
+python attack-multi-label-only.py --target vit-b --threshold s --multi_case 5 --deit False --is_transform True
+python attack-multi-label-only.py --target vit-b --threshold m --multi_case 5 --deit False --is_transform True
+python attack-multi-label-only.py --target vit-b --threshold l --multi_case 5 --deit False --is_transform True
+python attack-multi-label-only.py --target vit-b --threshold xl --multi_case 5 --deit False --is_transform True
+
+python attack-multi-label-only.py --target vit-t --threshold s --multi_case 5 --deit False --is_transform True
+python attack-multi-label-only.py --target vit-t --threshold m --multi_case 5 --deit False --is_transform True
+python attack-multi-label-only.py --target vit-t --threshold l --multi_case 5 --deit False --is_transform True
+python attack-multi-label-only.py --target vit-t --threshold xl --multi_case 5 --deit False --is_transform True
+
+
+python attack-multi-label-only.py --target vgg16 --threshold s --multi_case 5 --deit False --is_transform False
+python attack-multi-label-only.py --target vgg16 --threshold m --multi_case 5 --deit False --is_transform False
+python attack-multi-label-only.py --target vgg16 --threshold l --multi_case 5 --deit False --is_transform False
+python attack-multi-label-only.py --target vgg16 --threshold xl --multi_case 5 --deit False --is_transform False
+
+python attack-multi-label-only.py --target vgg19 --threshold s --multi_case 5 --deit False --is_transform False
+python attack-multi-label-only.py --target vgg19 --threshold m --multi_case 5 --deit False --is_transform False
+python attack-multi-label-only.py --target vgg19 --threshold l --multi_case 5 --deit False --is_transform False
+python attack-multi-label-only.py --target vgg19 --threshold xl --multi_case 5 --deit False --is_transform False
+
+python attack-multi-label-only.py --target resnet152 --threshold s --multi_case 5 --deit False --is_transform False
+python attack-multi-label-only.py --target resnet152 --threshold m --multi_case 5 --deit False --is_transform False
+python attack-multi-label-only.py --target resnet152 --threshold l --multi_case 5 --deit False --is_transform False
+python attack-multi-label-only.py --target resnet152 --threshold xl --multi_case 5 --deit False --is_transform False
